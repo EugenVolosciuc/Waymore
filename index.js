8cf8ed21408ce5d359747bf456e1b42efa6d1869
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2019-03-11 21:27:10 +0200

    Added sections, container and event listeners for menu.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const main = document.querySelector("main");
+
+// MENU BUTTON TOGGLE
 const btn = document.querySelector(".btn");
 const sideMenu = document.querySelector("#side-menu");
 
 sideMenu.setAttribute("style", "display: none");
-
 
 btn.addEventListener("click", function (event) {
     this.classList.toggle("active");
@@ -15,3 +17,51 @@ btn.addEventListener("click", function (event) {
         sideMenu.classList.toggle("menu-animation-appear");
     }
 })
+
+// CONTAINER
+const container = document.querySelector("#container");
+
+// I don't need this code, I guess
+// const nodeSections = document.querySelectorAll("section");
+
+// // Convert nodeSections to array
+// const sections = [];
+// for (let i = 0; i < nodeSections.length; i++) {
+//     var self = nodeSections[i];
+//     sections.push(self);
+// }
+
+function containerToggle(link) {
+    link.addEventListener("click", function (event) {
+        container.classList.remove("display-none");
+    })
+}
+
+// SECTION TOGGLE
+
+const links = document.querySelectorAll(".links");
+
+function sectionToggle(section) {
+    section.addEventListener("mousedown", function (event) {
+        containerToggle(section);
+        main.classList.add("darken");
+
+    })
+}
+
+for (let i = 0; i < links.length; i++) {
+    sectionToggle(links[i]);
+}
+
+// Go back to main
+const backButton = document.querySelector("#back");
+
+backButton.addEventListener("mousedown", (event) => {
+    container.classList.add("display-none");
+    main.classList.remove("darken");
+
+})
+
+// Todo:
+// 1. Clean up the code
+// 2. Hide menu when container shows
